48185840b8b2b06a9edfe9ee430cb551dfc7cc9f
danypriego <hpriego0@gmail.com>
2018-01-19 00:01:04 -0600

    creando submodulos

---
+++ ventasdb/src/ventasdb/Ventasdb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventasdb;
+
+/**
+ *
+ * @author invitado2
+ */
+public class Ventasdb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
